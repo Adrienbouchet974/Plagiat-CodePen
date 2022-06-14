25e72ed4686d35b0faf6d52088b37a608e5c9c9a
Adrien bouchet <adrien.bouchet.974@gmail.com>
2022-06-14 16:08:18 +0200

    Ajout de fonction pour le cadre js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,28 @@ const code = document.getElementById("result").contentWindow.document;
 function coding() {
     const html = document.getElementById("html");
     const css = document.getElementById("css");
+    const js = document.getElementById("js");
+    const new_style = document.createElement("style");
+    const new_script = document.createElement("script");
+    new_script.setAttribute("type", "text/javascript");
   
     html.onkeyup = function() {
         code.write(html.value);
         code.close();
     };
-    const new_style = document.createElement("style");
-
+    
     css.onkeyup = function() {
         const style = code.body;
         console.log(style.querySelector("style"));
         style.appendChild(new_style);
         new_style.textContent = `${css.value}`;
+    }
+    
+    js.onkeyup = function() {
+        const style = code.body;
+        console.log(style.querySelector("script"));
+        style.appendChild(new_script);
+        new_script.innerText = `${js.value}`;
     }
 }
 coding();
